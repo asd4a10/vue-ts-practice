66ec22aae0f1d8ec62450a5121d6db4c0a1ffe6e
asd4a10 <rustem.amanzholov@nu.edu.kz>
2023-07-10 00:25:01 +0600

    job type added, homepage started

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import Job from "./types/job.ts";
+
+const jobs = ref<Job[]>([
+  { title: "job title 1", location: "loc 1", salary: 1 },
+  { title: "job title 2", location: "2", salary: 1 },
+  { title: "job title 3", location: "3", salary: 1 },
+  { title: "job title 4", location: "4", salary: 1 },
+]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>Jobs</p>
+  <p>{{ jobs }}</p>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
